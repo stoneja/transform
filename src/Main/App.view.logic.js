ca700add425c1ed5be066f0aa6423b544eaa8e15
john <john.stone@btinternet.com>
2018-04-14 20:58:53 +0100

    latest

--- src/Main/App.view.logic.js
+++ src/Main/App.view.logic.js
@@ -2,7 +2,17 @@ import React from 'react'
 import App from './App.view.js'
 
 export default class AppLogic extends React.Component {
+state = {
+	isMain: true,
+isApply: false
+}
+
   render() {
-    return <App {...this.props} />
+    return <App {...this.props} {...this.state} next={() => {
+this.setState({
+	isMain: false,
+isApply: true
+})
+}}/>
   }
 }
